63d5910d947ba3cf8796e8989606d5b3305c5b60
donifez <donifez224@gmail.com>
2019-11-20 07:12:47 -0800

    changes

--- src/stageOne.js
+++ src/stageOne.js
@@ -57,51 +57,96 @@ class stageOne extends Component {
       
       <div className="form-row">
         
-        <div className="col-3">
-          <input type="text" className="form-control" placeholder="logo"/>
-        </div>
-        <div className="col-3">
+        
+        <div className="col-4">
           <input type="text" className="form-control" placeholder=" name"/>
           
         </div>
-        <div className="col-3">
+        <div className="col-4">
         <input type="text" class="form-control" id="inputPassword4" placeholder="Address"/>
           
         </div>
-        <div className="col-3 ">
+        <div className="col-4 ">
         <input type="text" class="form-control" id="inputPassword4" placeholder="State"/>
           
         </div>
 
 
-        <div className="col-3">
+        <div className="col-4">
           <input type="text" className="form-control" placeholder="country"/>
         </div>
-        <div className="col-3">
+        <div className="col-4">
           <input type="text" className="form-control" placeholder=" number of staff"/>
           
         </div>
-        <div className="col-3">
+        <div className="col-4">
         <input type="text" class="form-control" id="inputPassword4" placeholder="no of working days in a month"/>
           
         </div>
-        <div className="col-3 ">
+        <div className="col-6 select ">
         <select >
   <option value="volvo">Saturday</option>
   <option value="saab">Sunday</option>
+  <option value="volvo">Monday</option>
+  <option value="saab">Tuesday</option>
+  <option value="volvo">Wednesday</option>
+  <option value="saab">Thursday</option>
+  <option value="saab">Friday</option>
   
 </select>
+</div>
+<div className="col-6 select ">
+<p className="check"><input className="col-4 check "  type="checkbox" name="vehicle" value="Bike"/> Set day as default (optional)</p>
           
         </div>
+
+        <div className="col-12 file ">
+        <h6 className="logo">Select  logo:</h6>
+   <input className="file" type="file" name="myFile"/>
+  <input  className="file" type="submit"/>
+      </div>
+
     
        
       </div>
     </form>
 
+    <form>
+    <h4 className="user">Payment Details (Paystack) </h4>
+    <div className="form-row">
+    <div className="col-6">
+          <input type="number" className="form-control" placeholder=" card number"/>
+          
+        </div>
+        <div className="col-6">
+        <input type="date" class="form-control" id="inputPassword4" placeholder="Expiry date"/>
+          
+        </div>
+        <div className="col-6 ">
+        <input type="number" class="form-control" id="inputPassword4" placeholder="CVV"/>
+          
+        </div>
+        <div className="col-6 ">
+        <input type="number" class="form-control" id="inputPassword4" placeholder="amount"/>
+          
+        </div>
+        <div className="col-12 pin ">
+        <input  className="col-12 pin" type="number" class="form-control" id="inputPassword4" placeholder="PIN"/>
+          
+        </div>
+
+        <div className="col-12 ">
+        <button type="button" class="btn btn-success">Pay 300 NGN</button>
+          
+        </div>
+      
+        </div>
+    </form>
+
       </div>
       <div className="modal-footer">
         <button type="button" className="btn btn-secondary" data-dismiss="modal">Close</button>
-        <button type="button" className="btn btn-primary">next</button>
+        <button type="button" className="btn btn-primary">Submit form</button>
       </div>
     </div>
             

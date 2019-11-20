c451e25e915b2413abdc18ade42c85133b0be2a7
donifez <donifez224@gmail.com>
2019-11-20 05:18:11 -0800

    pushed

--- src/stageOne.js
+++ src/stageOne.js
@@ -1,5 +1,7 @@
 import React, {Component} from "react";
 import "./App.css";
+
+
 class stageOne extends Component {
     constructor(props) {
         super(props);
@@ -14,8 +16,8 @@ class stageOne extends Component {
               <div className="header">
               <h2>Simplify payroll</h2>
             <h3>With software and services designed for your business</h3>
-         
-<button type="button" className="btn btn-primary get-started" data-toggle="modal" data-target="#exampleModalScrollable">
+ 
+            <button type="button" className="btn btn-primary get-started" data-toggle="modal" data-target="#exampleModalScrollable">
   Get started
 </button>
 
@@ -23,9 +25,9 @@ class stageOne extends Component {
 <div className="modal fade" id="exampleModalScrollable" tabindex="-1" role="dialog" aria-labelledby="exampleModalScrollableTitle" aria-hidden="true">
   <div className="modal-dialog modal-dialog-scrollable" role="document">
     <div className="modal-content">
-    hdhhfhfad
+    
       <div className="modal-body">
-      <h2 className="user"> User Details</h2>
+      <h4 className="user"> User Details</h4>
       <form>
       
   <div className="form-row">
@@ -45,24 +47,69 @@ class stageOne extends Component {
     <input type="password" class="form-control" id="inputPassword4" placeholder="Password"/>
       
     </div>
+
+   
   </div>
 </form>
+<h4 className="user">Business Details</h4>
+
+<form>
+      
+      <div className="form-row">
+        
+        <div className="col-3">
+          <input type="text" className="form-control" placeholder="logo"/>
+        </div>
+        <div className="col-3">
+          <input type="text" className="form-control" placeholder=" name"/>
+          
+        </div>
+        <div className="col-3">
+        <input type="text" class="form-control" id="inputPassword4" placeholder="Address"/>
+          
+        </div>
+        <div className="col-3 ">
+        <input type="text" class="form-control" id="inputPassword4" placeholder="State"/>
+          
+        </div>
+
+
+        <div className="col-3">
+          <input type="text" className="form-control" placeholder="country"/>
+        </div>
+        <div className="col-3">
+          <input type="text" className="form-control" placeholder=" number of staff"/>
+          
+        </div>
+        <div className="col-3">
+        <input type="text" class="form-control" id="inputPassword4" placeholder="no of working days in a month"/>
+          
+        </div>
+        <div className="col-3 ">
+        <select >
+  <option value="volvo">Saturday</option>
+  <option value="saab">Sunday</option>
+  
+</select>
+          
+        </div>
+    
+       
+      </div>
+    </form>
+
       </div>
       <div className="modal-footer">
         <button type="button" className="btn btn-secondary" data-dismiss="modal">Close</button>
-        <button type="button" className="btn btn-primary">Save changes</button>
+        <button type="button" className="btn btn-primary">next</button>
       </div>
     </div>
-  </div>
-</div>
-        
-
-
-            </div>
             
             
           </div>
-          
+          </div>
+          </div>
+          </div>
           
           
         );

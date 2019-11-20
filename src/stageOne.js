bacc577df48034ec6e5785b06d35d7e1325aeff0
donifez <donifez224@gmail.com>
2019-11-20 00:18:21 -0800

    added

--- src/stageOne.js
+++ src/stageOne.js
@@ -15,21 +15,42 @@ class stageOne extends Component {
               <h2>Simplify payroll</h2>
             <h3>With software and services designed for your business</h3>
          
-<button type="button" class="btn btn-primary get-started" data-toggle="modal" data-target="#exampleModalScrollable">
+<button type="button" className="btn btn-primary get-started" data-toggle="modal" data-target="#exampleModalScrollable">
   Get started
 </button>
 
 
-<div class="modal fade" id="exampleModalScrollable" tabindex="-1" role="dialog" aria-labelledby="exampleModalScrollableTitle" aria-hidden="true">
-  <div class="modal-dialog modal-dialog-scrollable" role="document">
-    <div class="modal-content">
+<div className="modal fade" id="exampleModalScrollable" tabindex="-1" role="dialog" aria-labelledby="exampleModalScrollableTitle" aria-hidden="true">
+  <div className="modal-dialog modal-dialog-scrollable" role="document">
+    <div className="modal-content">
     hdhhfhfad
-      <div class="modal-body">
-      <p>dgskjdghkshgjsghsf</p>
+      <div className="modal-body">
+      <h2 className="user"> User Details</h2>
+      <form>
+      
+  <div className="form-row">
+    
+    <div className="col-6">
+      <input type="text" className="form-control" placeholder="First name"/>
+    </div>
+    <div className="col-6">
+      <input type="text" className="form-control" placeholder="Last name"/>
+      
+    </div>
+    <div className="col-6 space">
+    <input type="email" class="form-control" id="inputPassword4" placeholder="Email"/>
+      
+    </div>
+    <div className="col-6 space">
+    <input type="password" class="form-control" id="inputPassword4" placeholder="Password"/>
+      
+    </div>
+  </div>
+</form>
       </div>
-      <div class="modal-footer">
-        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-        <button type="button" class="btn btn-primary">Save changes</button>
+      <div className="modal-footer">
+        <button type="button" className="btn btn-secondary" data-dismiss="modal">Close</button>
+        <button type="button" className="btn btn-primary">Save changes</button>
       </div>
     </div>
   </div>
